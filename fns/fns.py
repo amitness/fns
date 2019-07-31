2047acd919618ee5847f8e18584decebf7b3ab48
Amit Chaudhary <meamitkc@gmail.com>
2019-07-31 15:17:11 +0545

    Add functions for primitive constructs

--- fns/fns.py
+++ fns/fns.py
@@ -1,2 +1,15 @@
-def test():
-	return "Boilerplate setup"
+from itertools import chain
+
+
+def flatten(x):
+    return chain.from_iterable(x)
+
+
+def sort_dict_by_value(d, reverse=False):
+    """
+    Sort items in dictionary by value.
+    
+    >>> sort_dict_by_value({'gold': 40, 'silver': 25})
+    {'silver': 25, 'gold': 40}
+    """
+    return dict(sorted(d.items(), key=lambda item: item[1], reverse=reverse))
