3a605d807bb3a48e8cbbea9c997bc8020c218d64
Amit Chaudhary <meamitkc@gmail.com>
2020-10-28 18:18:43 +0545

    Add method for viewing grid search results

--- fns/__init__.py
+++ fns/__init__.py
@@ -1,3 +1,3 @@
 from .fns import *
 
-__VERSION__ = '0.4.9'
+__VERSION__ = '0.4.10'
--- fns/model_selection.py
+++ fns/model_selection.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def train_val_size(dataset, val_ratio=0.1):
     """
     Return the train and validation data sizes based on split ratio.
@@ -8,3 +11,10 @@ def train_val_size(dataset, val_ratio=0.1):
     val_size = int(val_ratio * len(dataset))
     train_size = len(dataset) - val_size
     return train_size, val_size
+
+
+def view_result_table(cv):
+    columns = ['params', 'mean_test_score', 'std_test_score', 'rank_test_score']
+    return (pd.DataFrame(cv.cv_results_)[columns]
+            .sort_values(by=['rank_test_score'])
+            )
