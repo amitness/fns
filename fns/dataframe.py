9b6945bdacde140ebe55eb32d80cee475f375a5d
Amit Chaudhary <meamitkc@gmail.com>
2020-10-30 21:19:24 +0545

    Add helper method to show all rows and columns in a pandas dataframe

--- fns/dataframe.py
+++ fns/dataframe.py
@@ -10,3 +10,35 @@ def read_dict(data: Dict):
     :return: Pandas DataFrame
     """
     return pd.DataFrame.from_dict(data, orient='index').transpose()
+
+
+def print_groups(df: pd.DataFrame, column: str) -> None:
+    """
+    Pretty print
+    Parameters
+    ----------
+    df: Pandas DataFrame
+    column: Column Name to group by
+
+    Returns
+    -------
+
+    """
+    for current_group, sub_df in df.groupby(column):
+        print(f'Group: {current_group}')
+        print()
+        print(sub_df)
+        print()
+        print('---' * 25)
+
+
+def display_all() -> None:
+    """
+    Show all the rows and columns when printing dataframe.
+
+    Returns None
+    -------
+    """
+    import pandas as pd
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_rows', None)
