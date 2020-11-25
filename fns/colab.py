bc6f664ffd5a48ed02f0ceb21785d30b7a2f8edd
Amit Chaudhary <meamitkc@gmail.com>
2020-11-25 16:06:01 +0545

    Add command to start jupyter notebook server on colab

---
+++ fns/colab.py
@@ -0,0 +1,25 @@
+import subprocess
+
+
+def run_foreground(cmd):
+    """from http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html
+    """
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = []
+    while True:
+        line = p.stdout.readline()
+        stdout.append(line)
+        print(line.strip())
+        if line == '' and p.poll() != None:
+            break
+    return None
+
+
+def run_background(command):
+    subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+
+
+def jupyter():
+    command = 'nohup jupyter notebook --no-browser --allow-root --ip="127.0.0.1" --port="6006" &'
+    run_background(command)
+    run_foreground('npx localtunnel --port 6006')
