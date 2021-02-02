89c7a6798ff0963ed3e8c7e4ef38217338cbfdf2
Amit Chaudhary <meamitkc@gmail.com>
2021-02-02 21:45:38 +0545

    Check python version 3.7

--- setup.py
+++ setup.py
@@ -8,8 +8,13 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     classifiers=[
-        'Programming Language :: Python :: 3'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
+    python_requires=">=3.7",
     author='Amit Chaudhary',
     author_email='meamitkc@gmail.com',
     url='https://github.com/amitness/fns',
