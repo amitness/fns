8411dd16495cee742e5d2b68558666080ea67704
Amit Chaudhary <meamitkc@gmail.com>
2021-03-31 18:14:14 +0545

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fns',
-    version='0.4.69',
+    version='0.4.70',
     license='MIT',
     description='Reusable functions for use in ML projects',
     long_description=open('README.md').read(),
