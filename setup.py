f6a99c688a120530788d9059d6de3c84444bd086
Amit Chaudhary <meamitkc@gmail.com>
2021-04-14 20:01:17 +0545

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fns',
-    version='0.4.81',
+    version='0.4.82',
     license='MIT',
     description='Reusable functions for use in ML projects',
     long_description=open('README.md').read(),
