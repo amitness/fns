f854f95200ab35c500ca78723085e834db71d643
Amit Chaudhary <meamitkc@gmail.com>
2021-05-08 17:56:13 +0545

    Add transformer for Histogram based featurization of text

---
+++ fns/feature_extraction.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+def encode_histogram(text: str) -> np.ndarray:
+    histogram = [0.0] * 256
+    for ascii_code in text.encode('utf-8'):
+        histogram[ascii_code] += 1
+    return np.array(histogram) / len(text)
+
+
+class HistogramEncoder(BaseEstimator, TransformerMixin):
+    """
+    Encode text into a bucket of 256 byte indices.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def fit(self, *args, **kwargs):
+        return self
+
+    def transform(self, x):
+        return np.stack([encode_histogram(e) for e in x])
---
+++ tests/test_feature_extraction.py
@@ -0,0 +1,7 @@
+from fns.feature_extraction import HistogramEncoder
+
+
+def test_histogram_encoder():
+    encoder = HistogramEncoder()
+    output = encoder.fit_transform(['hello', 'world'])
+    assert output.shape == (2, 256)
